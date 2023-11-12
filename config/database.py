0012105a18d559452c0ac5210eae2815feda777a
eidde606 <eidde_606@yahoo.com>
2023-11-12 16:47:27 -0500

    made changes to the ssl

--- config/database.py
+++ config/database.py
@@ -1,11 +1,8 @@
 from pymongo.mongo_client import MongoClient
 
-import ssl
 
 client = MongoClient(
     "mongodb+srv://eidde606:edian606@cluster0.h9pamtb.mongodb.net/?retryWrites=true&w=majority",
-    ssl=True,
-    ssl_cert_reqs=ssl.CERT_NONE,
 )
 db = client.TicketDB
 collection_name = db["tickets"]
