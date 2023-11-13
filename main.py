488186b6b7a18b92d8f9a0b4480237f586460c2d
eidde606 <eidde_606@yahoo.com>
2023-11-13 00:32:41 -0500

    changes

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.route import router
 
 app = FastAPI()
+
+# Set up CORS
+origins = [
+    "https://ephemeral-bubblegum-4b0b0c.netlify.app",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["Content-Type", "Authorization"],
+)
 app.include_router(router)
