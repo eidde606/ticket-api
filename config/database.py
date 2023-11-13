48a08bb2728d685f8feff9526a64c3a5618cd88b
eidde606 <eidde_606@yahoo.com>
2023-11-12 22:42:49 -0500

    changes

--- config/database.py
+++ config/database.py
@@ -4,8 +4,6 @@ import ssl
 
 client = MongoClient(
     "mongodb+srv://eidde606:edian606@cluster0.h9pamtb.mongodb.net/?retryWrites=true&w=majority",
-    ssl=True,
-    ssl_ca_certs="path/to/ca-certificate.pem",
 )
 db = client.TicketDB
 collection_name = db["tickets"]
